83c59e30d39d770a2685fb9fdace7bcb108c9a59
Kenier Becerra Becerra <kenier.becerra@siigroup.cl>
2026-06-01 17:58:21 -0400

    revisar mongo uri

--- database/connection.js
+++ database/connection.js
@@ -5,10 +5,9 @@ mongoose.set("strictQuery", true);
 // Configurar conexion a base de datos
 const connection = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-
     console.log("URI definida:", !!process.env.MONGO_URI);
     console.log("URI empieza por:", process.env.MONGO_URI?.substring(0, 25));
+    await mongoose.connect(process.env.MONGO_URI);
 
     console.log("MongoDB conectado correctament");
   } catch (error) {
